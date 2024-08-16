4773ca3950d0bfa1b37cfda75f93c9427dcda229
Md. Ismail Hossain <hossain.eee042@gmail.com>
2024-08-16 12:57:52 +0600

    image asset class has been added

---
+++ lib/presentation/ui/utility/image_asset.dart
@@ -0,0 +1,7 @@
+class ImageAssets{
+  ImageAssets._(); //singleton
+  static const String _imagePath ="assets/images";
+  static const String desertImage = "$_imagePath/desert.jpg";
+  static const String mosqueImage = "$_imagePath/mosque.jpg";
+
+}
