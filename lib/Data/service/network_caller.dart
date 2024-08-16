ae243c7faa1585f0a7a32f09cf7eade17831b01b
Md. Ismail Hossain <hossain.eee042@gmail.com>
2024-08-16 16:12:47 +0600

    add error snackbar message in network caller model

--- lib/Data/service/network_caller.dart
+++ lib/Data/service/network_caller.dart
@@ -6,10 +6,13 @@ import 'package:http/http.dart' as http;
 import 'package:task_1_api_data_fetch/Data/model/network_response.dart';
 
 class NetworkCaller {
+  
 //get request
-  static Future<NetworkResponse> getRequest(String url,) async {
+  static Future<NetworkResponse> getRequest(
+    String url,
+  ) async {
     try {
-     http. Response response = await http.get(Uri.parse(url));
+      http.Response response = await http.get(Uri.parse(url));
       // headers: {'token': AuthController.accessToken.toString()});
 
       log(response.statusCode.toString());
@@ -19,34 +22,30 @@ class NetworkCaller {
         return NetworkResponse(
             true, response.statusCode, jsonDecode(response.body));
       } else if (response.statusCode == 401) {
-      
-          // gotoLogin();
-          Get.snackbar(
-            'Error', // Title
-            'Authentication failed', // Message
-            snackPosition: SnackPosition.BOTTOM, // Position
-            duration: Duration(seconds: 2), // Duration
-            backgroundColor: Colors.blue, // Background color
-            colorText: Colors.white, // Text color
-            borderRadius: 10, // Border radius
-          );
-
-     
+        // gotoLogin();
+        Get.snackbar(
+          'Error', // Title
+          'Authentication failed', // Message
+          snackPosition: SnackPosition.BOTTOM, // Position
+          duration: Duration(seconds: 2), // Duration
+          backgroundColor: Colors.blue, // Background color
+          colorText: Colors.white, // Text color
+          borderRadius: 10, // Border radius
+        );
       } else {
         return NetworkResponse(false, response.statusCode, null);
       }
     } catch (e) {
-
       log(e.toString());
       Get.snackbar(
-            'Error', // Title
-            'Something went wrong', // Message
-            snackPosition: SnackPosition.BOTTOM, // Position
-            duration: Duration(seconds: 2), // Duration
-            backgroundColor: Colors.red[300], // Background color
-            colorText: Colors.white, // Text color
-            borderRadius: 10, // Border radius
-          );
+        'Error', // Title
+        'Something went wrong', // Message
+        snackPosition: SnackPosition.BOTTOM, // Position
+        duration: Duration(seconds: 2), // Duration
+        backgroundColor: Colors.red[300], // Background color
+        colorText: Colors.white, // Text color
+        borderRadius: 10, // Border radius
+      );
     }
     return NetworkResponse(false, -1, null);
   }
